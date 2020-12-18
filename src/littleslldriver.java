75419c193225f91f0ee621cfa54fed187ae4716a
mgurga <motiurga@gmail.com>
2020-12-17 18:12:25 -0800

    created driver class

--- src/littleslldriver.java
+++ src/littleslldriver.java
@@ -1,6 +1,17 @@
 
 public class littleslldriver {
 	public static void main(String[] args) {
-		
+		littlesll sll = new littlesll();
+		sll.addNodeToStart(5);
+		sll.addNodeToStart(52);
+		sll.addNodeToStart(-20);
+        
+		sll.showList();
+        System.out.println("The list length is: " + sll.length());
+        System.out.println();
+        
+        sll.deleteNodeFromStart();
+        sll.showList();
+        System.out.println("The list length is now: " + sll.length());
 	}
 }
